0c855d9a77c58ebc0bac78f1a1f95b742f5005da
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2021-01-23 14:02:13 +0300

    Create Musteri.cs

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Id { get; set; }
+        public double Tc_No { get; set; }
+    }
+}
